40029b2cad6ca63f9f001446bf473a1012c12403
Emil Wasilewski <emil.wasilewski@gmail.com>
2018-06-07 23:42:55 +0200

    Add files via upload

---
+++ PowerShell/LoginToAzureSPN.ps1
@@ -0,0 +1,8 @@
+﻿$client_id = "application ID"
+$secret = "Application Key or provided password"
+$tenant = "Tenat ID"
+
+$pass = ConvertTo-SecureString $secret -AsPlainText –Force
+$cred = New-Object -TypeName pscredential –ArgumentList $client_id, $pass
+
+Login-AzureRmAccount -Credential $cred -ServicePrincipal -TenantId $tenant
