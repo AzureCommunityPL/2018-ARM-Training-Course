3f99f1997153db14591d6345996cc3975c430273
michal_s <michal@aegis.org.pl>
2018-06-08 20:12:34 +0200

    change to md

--- PowerShell/LoginToAzureSPN.ps1
+++
@@ -1,8 +0,0 @@
-﻿$client_id = "application ID"
-$secret = "Application Key or provided password"
-$tenant = "Tenat ID"
-
-$pass = ConvertTo-SecureString $secret -AsPlainText –Force
-$cred = New-Object -TypeName pscredential –ArgumentList $client_id, $pass
-
-Login-AzureRmAccount -Credential $cred -ServicePrincipal -TenantId $tenant
